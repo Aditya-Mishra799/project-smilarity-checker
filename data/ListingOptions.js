bc3944d95a001e9a467ae00c211685eed4e17a4f
Aditya-Mishra799 <adityamishra9124@gmail.com>
2025-02-17 12:58:42 +0530

    fixed build issues

---
+++ data/ListingOptions.js
@@ -0,0 +1,57 @@
+const propertyOptions = [
+    "Apartment",
+    "House",
+    "Villa",
+    "Condo",
+    "Townhouse",
+    "Duplex",
+    "Penthouse",
+    "Studio",
+    "Loft",
+    "Farm House",
+    "Independent",
+    "Residential Land",
+  ];
+  
+  const amenitiesOptions = [
+    "Air Conditioning",
+    "Balcony",
+    "Swimming Pool",
+    "Gym",
+    "Parking",
+    "Elevator",
+    "Security System",
+    "Fireplace",
+    "Garden",
+    "Furnished",
+    "Unfurnished",
+    "Semi-Furnished",
+    "Pet Friendly",
+    "Laundry Room",
+    "Central Heating",
+    "Walk-in Closet",
+    "View",
+    "Kitchen Appliances",
+    "24/7 Security",
+    "Playground",
+    "Spa",
+  ];
+  
+  const furnishedStatusOptions = [
+    "Furnished",
+    "Semi-Furnished",
+    "Unfurnished",
+  ];
+  
+  const listingTypeOptions = [
+    "For Rent",
+    "For Sale",
+  ];
+  
+  export {
+    propertyOptions,
+    amenitiesOptions,
+    furnishedStatusOptions,
+    listingTypeOptions,
+  };
+  
