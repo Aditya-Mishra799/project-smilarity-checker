ab442b02d566c72c3b87405166f01f6d0c888944
Aditya-Mishra799 <adityamishra9124@gmail.com>
2025-02-21 09:17:42 +0530

    added project icon

--- components/header/Logo.jsx
+++ components/header/Logo.jsx
@@ -1,13 +1,19 @@
-import { HousePlus, Notebook } from 'lucide-react'
-import React from 'react'
+import { HousePlus, Notebook } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+import icon from "@/public/favicon.png"
 
 const Logo = () => {
   return (
-    <div className='flex gap-2 items-center'>
-        <h1 className='text-xl font-bold tracking-wider uppercase text-gray-800 md:text-2xl'>Project Manager</h1>
-        <Notebook className="w-6 h-6 text-indigo-500 md:w-8 md:h-8" />
-    </div>
-  )
-}
+    <div className="flex gap-2 items-center">
+      <h1 className="text-base font-bold tracking-wider uppercase text-gray-800 sm:text-xl md:text-2xl">
+        Project Manager
+      </h1>
+      {/* <Notebook className="w-6 h-6 text-indigo-500 md:w-8 md:h-8" /> */}
 
-export default Logo
+      <Image src={icon} alt="logo" width={26} height={26} />
+    </div>
+  );
+};
+
+export default Logo;
