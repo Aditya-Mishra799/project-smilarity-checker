0c8a43ba825b6e3333e075d03c8aefa0691d1630
Aditya-Mishra799 <adityamishra@eng.rizvi.edu.in>
2025-04-02 15:48:27 +0530

    Added privilege: Only Admins can create sessions

--- app/session/upsert/page.jsx
+++ app/session/upsert/page.jsx
@@ -1,5 +1,7 @@
+import authOptions from "@/app/api/auth/[...nextauth]/nextAuthOptions";
 import AttachSessionDetails from "@/components/AttachSessionDetails";
 import { getSessionById } from "@/server-actions/sessionAction";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import React from "react";
 import UpsertSession from "./UpsertSession";
@@ -7,6 +9,11 @@ import UpsertSession from "./UpsertSession";
 const page = async ({ searchParams }) => {
   const id = (await searchParams).id;
   let defaultValue = {};
+  const { user } = await getServerSession(authOptions);
+  const validRoles = ["admin", "super-admin"];
+  if (!validRoles.includes(user?.role)) {
+    return notFound();
+  }
   if (id && id != "") {
     const response = await getSessionById(id);
     if (!response.success) {
--- components/header/ProfileMenu.jsx
+++ components/header/ProfileMenu.jsx
@@ -1,22 +1,30 @@
-import { User } from 'lucide-react';
-import { signOut } from 'next-auth/react';
-import Link from 'next/link';
-import React from 'react';
+import { User } from "lucide-react";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 const ProfileMenu = ({ user }) => {
   if (!user) {
     return null; // Return nothing if the user object is not provided
   }
+  const validRoles = ["admin", "super-admin"];
 
   return (
     <div className="p-4 bg-white shadow-lg rounded-md w-64">
       {/* User Info */}
       <Link className="flex items-center gap-3 mb-4" href={"/profile"}>
-      {user?.image ? <img
-          src={user?.image}
-          alt="User Avatar"
-          className="w-12 h-12 rounded-full border-2 border-blue-500"
-        />:  <User className=" text-gray-400 rounded-full border-2 border-blue-500" size = {36} />}
+        {user?.image ? (
+          <img
+            src={user?.image}
+            alt="User Avatar"
+            className="w-12 h-12 rounded-full border-2 border-blue-500"
+          />
+        ) : (
+          <User
+            className=" text-gray-400 rounded-full border-2 border-blue-500"
+            size={36}
+          />
+        )}
         <div>
           <p className="text-gray-800 font-semibold text-base">{user.name}</p>
           <p className="text-gray-500 text-xs tracking-wide">{user.email}</p>
@@ -25,16 +33,18 @@ const ProfileMenu = ({ user }) => {
 
       {/* Links */}
       <div className="space-y-2 text-sm">
-        <Link
-          href="/session/upsert"
-          className="block text-blue-600 hover:text-blue-800 font-medium transition"
-        >
-          Create Session
-        </Link>
+        {validRoles.includes(user?.role) && (
+          <Link
+            href="/session/upsert"
+            className="block text-blue-600 hover:text-blue-800 font-medium transition"
+          >
+            Create Session
+          </Link>
+        )}
         <button
           href="/logout"
           className="block text-red-500 hover:text-red-700 font-medium transition"
-          onClick={()=>signOut({ callbackUrl: '/', redirect:true })}
+          onClick={() => signOut({ callbackUrl: "/", redirect: true })}
         >
           Logout
         </button>
